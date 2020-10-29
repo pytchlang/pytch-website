f670df938da7528dca042522eba9414e4b84daef
Ben North <ben@redfrontdoor.org>
2020-10-29 07:11:04 +0000

    Clean make script: Check for sibling repos

--- bin/make-cleanly.sh
+++ bin/make-cleanly.sh
@@ -12,6 +12,18 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "$REPO_ROOT" && (
     echo Working in $(pwd)
 
+    setup_ok=yes
+    for lnk in vm webapp; do
+        if [ ! -e source/$lnk/index.rst ]; then
+            echo "No index.rst found under source/$lnk; check sibling repos"
+            setup_ok=no
+        fi
+    done
+    if [ $setup_ok = no ]; then
+        echo "Abandoning clean build"
+        exit 1;
+    fi
+
     . venv/bin/activate
 
     echo
