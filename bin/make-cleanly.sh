2565d01d34ef7d8eda8bc5225ebf9f7a74375c02
Ben North <ben@redfrontdoor.org>
2020-10-29 07:16:06 +0000

    Clean make script: Check for venv

--- bin/make-cleanly.sh
+++ bin/make-cleanly.sh
@@ -19,6 +19,12 @@ cd "$REPO_ROOT" && (
             setup_ok=no
         fi
     done
+
+    if [ ! -e venv/bin/python ]; then
+        echo "No Python virtualenv found in 'venv'"
+        setup_ok=no
+    fi
+
     if [ $setup_ok = no ]; then
         echo "Abandoning clean build"
         exit 1;
