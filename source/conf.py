aaa730c2f1da3d643832aeb2e91eaafcac78772b
Ben North <ben@redfrontdoor.org>
2020-10-29 07:45:05 +0000

    Do not include source reST files in build

--- source/conf.py
+++ source/conf.py
@@ -45,6 +45,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "classic"
+html_copy_source = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
